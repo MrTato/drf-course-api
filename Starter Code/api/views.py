e5646c42ce5031271d949a944d43c5f7ebb438c1
Bayardo Lopez <bjlslopez@gmail.com>
2025-04-14 05:47:14 -0600

    django-silk for Profiling and Optimization with Django REST Framework

--- Starter Code/api/views.py
+++ Starter Code/api/views.py
@@ -12,17 +12,20 @@ def product_list(request):
     serializer = ProductSerializer(products, many=True)
     return Response(serializer.data)
 
+
 @api_view(['GET'])
 def product_detail(request, pk):
     product = get_object_or_404(Product, pk=pk)
     serializer = ProductSerializer(product)
     return Response(serializer.data)
 
+
 @api_view(['GET'])
 def order_list(request):
-    orders = Order.objects.all()
+    orders = Order.objects.prefetch_related('items__product')
     serializer = OrderSerializer(orders, many=True)
     return Response(serializer.data)
+
 
 @api_view(['GET'])
 def product_info(request):
