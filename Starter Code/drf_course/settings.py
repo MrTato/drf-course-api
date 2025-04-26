f879575523e6800303b774d5f2eec8b93e2e25b5
Bayardo Lopez <bjlslopez@gmail.com>
2025-04-26 09:11:15 -0600

    Django REST Framework API Documentation

--- Starter Code/drf_course/settings.py
+++ Starter Code/drf_course/settings.py
@@ -28,7 +28,8 @@ INSTALLED_APPS = [
     'django_extensions',
     'api',
     'rest_framework',
-    'silk'
+    'silk',
+    'drf_spectacular'
 ]
 
 MIDDLEWARE = [
@@ -120,6 +121,13 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication',
         'rest_framework.authentication.SessionAuthentication',
+    ],
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema'
+}
 
-    ]
+SPECTACULAR_SETTINGS = {
+    'TITLE': 'Tutorial API',
+    'DESCRIPTION': 'A simple tutorial project that helps in learning DRF',
+    'VERSION': '1.0.0',
+    'SERVE_INCLUDE_SCHEMA': False,
 }
